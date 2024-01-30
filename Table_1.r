a709420c41e18ab84df036e3b93198a11e1f0f6e
Felipe Rojas-Rodríguez <afeliperojas94@gmail.com>
2024-01-30 17:44:07 +0100

    Update table1

--- Table_1.r
+++ Table_1.r
@@ -1,48 +1,43 @@
 # TABLE 1
 # Baseline characteristics among IHC markers datasets (Table 1)
-```{r COUNT_PATIENTS}
-IDS_by_marker <- rbind(CD8_tils_BRIDGES %>% select(BCAC_ID), 
-      FOXP3_tils_BRIDGES %>% select(BCAC_ID), 
-      CD20_tils_BRIDGES %>% select(BCAC_ID),
-      CD163_tils_BRIDGES %>% select(BCAC_ID)) %>%
-  distinct(BCAC_ID) %>%
-  mutate(CD8 = case_when(BCAC_ID %in% CD8_tils_BRIDGES$BCAC_ID ~ 1, TRUE ~ 0),
-         FOXP3 = case_when(BCAC_ID %in% FOXP3_tils_BRIDGES$BCAC_ID ~ 1, TRUE ~ 0),
-         CD20 = case_when(BCAC_ID %in% CD20_tils_BRIDGES$BCAC_ID ~ 1, TRUE ~ 0),
-         CD163 = case_when(BCAC_ID %in% CD163_tils_BRIDGES$BCAC_ID ~ 1, TRUE ~ 0)
-  )
+dataset_ids_presence <- TilScores_BridgesPhenotype_markers %>% select(BCAC_ID, marker) %>%
+  mutate(presence = 1) %>%
+  pivot_wider(names_from = "marker", values_from = "presence", values_fill = 0) %>%
+  mutate(all = 1)
+  
+# Samples with all markers available
+all_markers <- filter(.data = dataset_ids_presence, CD8 == 1 & FOXP3 == 1 & CD20 == 1 & CD163 == 1)$BCAC_ID
 
-all_markers <- IDS_by_marker %>% filter(CD8 == 1 & FOXP3 == 1 & CD20 == 1 & CD163 == 1) %>% .$BCAC_ID
-any_markers <- IDS_by_marker %>% filter(CD8 == 1 | FOXP3 == 1 | CD20 == 1 | CD163 == 1) %>% .$BCAC_ID
-length(all_markers)
-length(any_markers)
+# All samples included with any of the markers
+any_markers <- filter(.data = dataset_ids_presence, all == 1)$BCAC_ID
 
+
+table(phenotype_BRIDGES_fortables$ER_statusIndex, useNA = "always")
 
 phenotype_BRIDGES_fortables <- read_tsv("/DATA/users/f.rojas/Projects/immune_infiltration_germline/official_data_BCAC_06122022/concept_734_schmidt_euro_cases_bridges_pheno_v15_02.txt") %>%
   select(BRIDGES_ID, BCAC_ID, study, AgeDiagIndex, Index_corr, BehaviourIndex, GradeIndex, 
          MorphologygroupIndex_corr, NodeStatusIndex, NodesIndex, SizeIndex_Cat, 
-         ER_statusIndex, PR_statusIndex, HER2_statusIndex) %>%
-  # filter(BCAC_ID %in% any_markers) %>%
-  mutate(all_markers_ids = case_when(BCAC_ID %in% all_markers ~ 1, TRUE ~ 0)) %>%
-  mutate(AgeDiagIndex = case_when(AgeDiagIndex == 888 ~ NA_real_, TRUE ~ AgeDiagIndex),
-         BehaviourIndex = case_when(BehaviourIndex == 1 ~ "Invasive",
+         ER_statusIndex, PR_statusIndex, HER2_statusIndex)
+
+phenotype_BRIDGES_fortables[phenotype_BRIDGES_fortables == 888] <- NA
+
+
+phenotype_BRIDGES_fortables <- phenotype_BRIDGES_fortables %>%
+  mutate(all_markers_ids = case_when(BCAC_ID %in% all_markers ~ 1, TRUE ~ 0),
+         any_markers_ids = case_when(BCAC_ID %in% any_markers ~ 1, TRUE ~ 0)) %>%
+  filter(any_markers_ids == 1) %>%
+  mutate(BehaviourIndex = case_when(BehaviourIndex == 1 ~ "Invasive",
                                     BehaviourIndex == 2 ~ "In-situ",
                                     BehaviourIndex == 888 ~ NA_character_),
-         GradeIndex = case_when(GradeIndex == 888 ~ NA_real_, TRUE ~ GradeIndex),
          MorphologygroupIndex_corr = case_when(MorphologygroupIndex_corr == "Mixed (ductal & lobular)" ~ "Mixed",
                                                MorphologygroupIndex_corr %in% c("Medullary", "Mucinous", 
                                                                                 "Other", "Papillary", "Tubular") ~ "Other",
-                                               MorphologygroupIndex_corr == 888 ~ NA_character_,
                                                TRUE ~ MorphologygroupIndex_corr),
-         NodeStatusIndex = case_when(NodeStatusIndex == 888 ~ NA_real_, TRUE ~ NodeStatusIndex),
-         SizeIndex_Cat = case_when(SizeIndex_Cat == 888 ~ NA_real_, TRUE ~ SizeIndex_Cat),
-         ER_statusIndex = case_when(ER_statusIndex == 888 ~ NA_real_, TRUE ~ ER_statusIndex),
-         PR_statusIndex = case_when(PR_statusIndex == 888 ~ NA_real_, TRUE ~ PR_statusIndex),
-         HER2_statusIndex = case_when(HER2_statusIndex == 888 ~ NA_real_, TRUE ~ HER2_statusIndex),
-         CD8 = case_when(BCAC_ID %in% CD8_tils_BRIDGES$BCAC_ID ~ 1, TRUE ~ 0),
-         FOXP3 = case_when(BCAC_ID %in% FOXP3_tils_BRIDGES$BCAC_ID ~ 1, TRUE ~ 0),
-         CD20 = case_when(BCAC_ID %in% CD20_tils_BRIDGES$BCAC_ID ~ 1, TRUE ~ 0),
-         CD163 = case_when(BCAC_ID %in% CD163_tils_BRIDGES$BCAC_ID ~ 1, TRUE ~ 0)) %>% 
+         `All cases` = case_when(BCAC_ID %in% filter(dataset_ids_presence, all == 1)$BCAC_ID ~ 1, TRUE ~ 0),
+         CD8 = case_when(BCAC_ID %in% filter(dataset_ids_presence, CD8 == 1)$BCAC_ID ~ 1, TRUE ~ 0),
+         FOXP3 = case_when(BCAC_ID %in% filter(dataset_ids_presence, FOXP3 == 1)$BCAC_ID ~ 1, TRUE ~ 0),
+         CD20 = case_when(BCAC_ID %in% filter(dataset_ids_presence, CD20 == 1)$BCAC_ID ~ 1, TRUE ~ 0),
+         CD163 = case_when(BCAC_ID %in% filter(dataset_ids_presence, CD163 == 1)$BCAC_ID ~ 1, TRUE ~ 0)) %>% 
   mutate(BehaviourIndex = factor(BehaviourIndex, levels = c("In-situ", "Invasive"),
                                  labels = c("In-situ", "Invasive")),
          GradeIndex = factor(GradeIndex, levels = c(1, 2, 3),
@@ -58,52 +53,31 @@ phenotype_BRIDGES_fortables <- read_tsv("/DATA/users/f.rojas/Projects/immune_inf
          PR_statusIndex = factor(PR_statusIndex, levels = c(0,1), 
                                  labels = c("Negative", "Positive")),
          HER2_statusIndex = factor(HER2_statusIndex, levels = c(0,1), 
-                                   labels = c("Negative", "Positive")),
-         all_markers_ids = factor(all_markers_ids, levels = c(0,1),
-                                  labels = c("Not all Markers", "All Markers"))
+                                   labels = c("Negative", "Positive"))
          )
 
 #### ####
-table1::label(phenotype_BRIDGES_fortables$AgeDiagIndex) <- "Age"
+table1::label(phenotype_BRIDGES_fortables$AgeDiagIndex) <- "Age, years"
 table1::label(phenotype_BRIDGES_fortables$BehaviourIndex) <- "Behaviour"
 table1::label(phenotype_BRIDGES_fortables$GradeIndex) <- "Grade"
 table1::label(phenotype_BRIDGES_fortables$MorphologygroupIndex_corr) <- "Morphology"
 table1::label(phenotype_BRIDGES_fortables$NodeStatusIndex) <- "Node Status"
-table1::label(phenotype_BRIDGES_fortables$SizeIndex_Cat) <- "Size"
-table1::label(phenotype_BRIDGES_fortables$ER_statusIndex) <- "ER"
-table1::label(phenotype_BRIDGES_fortables$PR_statusIndex) <- "PR"
-table1::label(phenotype_BRIDGES_fortables$HER2_statusIndex) <- "HER2"
-table1::label(phenotype_BRIDGES_fortables$all_markers_ids) <- "Markers"
-
-
-pvalue <- function(x, ...) {
-    # Construct vectors of data y, and groups (strata) g
-    y <- unlist(x)
-    g <- factor(rep(1:length(x), times=sapply(x, length)))
-    if (is.numeric(y)) {
-        # For numeric variables, perform a standard 2-sample t-test
-        p <- t.test(y ~ g)$p.value
-    } else {
-        # For categorical variables, perform a chi-squared test of independence
-        p <- chisq.test(table(y, g))$p.value
-    }
-    # Format the p-value, using an HTML entity for the less-than sign.
-    # The initial empty string places the output on the line below the variable label.
-    c("", sub("<", "&lt;", format.pval(p, digits=3, eps=0.001)))
-    }
-
+table1::label(phenotype_BRIDGES_fortables$SizeIndex_Cat) <- "Tumor size"
+table1::label(phenotype_BRIDGES_fortables$ER_statusIndex) <- "ER-status"
+table1::label(phenotype_BRIDGES_fortables$PR_statusIndex) <- "PR-status"
+table1::label(phenotype_BRIDGES_fortables$HER2_statusIndex) <- "HER2-status"
 
 overall_table1 <- phenotype_BRIDGES_fortables %>%
+  dplyr::select(-c(all_markers_ids, any_markers_ids)) %>%
   pivot_longer(-c(BRIDGES_ID, BCAC_ID, study, AgeDiagIndex, Index_corr, 
                   BehaviourIndex, GradeIndex, MorphologygroupIndex_corr, 
                   NodeStatusIndex, NodesIndex, SizeIndex_Cat, 
-                  ER_statusIndex, PR_statusIndex, HER2_statusIndex, all_markers_ids), 
+                  ER_statusIndex, PR_statusIndex, HER2_statusIndex),
                names_to = "markers", values_to = "carrier") %>%
-  filter(carrier == 1) %>%
-  mutate(markers = factor(markers, levels = c("CD8", "CD20", "FOXP3", "CD163")))
+  mutate(markers = factor(markers, levels = c("All cases", "CD8", "CD20", "FOXP3", "CD163"))) %>%
+  filter(carrier != 0)
 
-table1::table1(~ AgeDiagIndex + ER_statusIndex + HER2_statusIndex + GradeIndex + MorphologygroupIndex_corr + NodeStatusIndex + BehaviourIndex + SizeIndex_Cat | markers, 
+
+table1::table1(~ AgeDiagIndex + ER_statusIndex + PR_statusIndex + HER2_statusIndex + GradeIndex + MorphologygroupIndex_corr + NodeStatusIndex + BehaviourIndex + SizeIndex_Cat | markers, 
                data = overall_table1, overall = FALSE,
                topclass = "Rtable1-zebra")
-
-```
